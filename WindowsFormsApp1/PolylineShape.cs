c2d19a9a52f16b5d6887a0efd14dbaec182816cc
Arseniy Komar <arsenjik12@gmail.com>
2025-02-27 14:00:32 +0300

    Lab1, classes in the separate files

---
+++ WindowsFormsApp1/PolylineShape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class PolylineShape : Shape
+    {
+        private Point[] points;
+
+        public PolylineShape(Point[] points, Color color, float width) : base(color, width)
+        {
+            this.points = points;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            g.DrawLines(pen, points);
+        }
+    }
+}
